c75d49be8ad7ef434938dd7e94826b8a56a1266d
aguitarhero7-arch <aguitarhero7@gmail.com>
2026-09-23 01:51:25 +0000

    add manual import support

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask,render_template,jsonify
 from database import connect
 from scanner import scan
+
 app=Flask(__name__)
 @app.get('/')
 def home(): return render_template('index.html',listings=connect().execute('SELECT * FROM listings ORDER BY first_seen DESC').fetchall())
 @app.get('/api/listings')
 def api(): return jsonify([dict(x) for x in connect().execute('SELECT * FROM listings ORDER BY first_seen DESC').fetchall()])
 if __name__=='__main__': app.run(host='0.0.0.0',port=5000)
+
