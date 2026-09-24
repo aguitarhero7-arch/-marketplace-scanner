657965556ad7d66766e868df0c9b17d2d771f906
aguitarhero7-arch <aguitarhero7@gmail.com>
2026-09-24 20:56:23 +0000

    Connect API token

--- sources/marketplace_source.py
+++ sources/marketplace_source.py
@@ -1,8 +1,11 @@
+import os 
 from models import Listing
+import requests
+APIFY_TOKEN = os.getenv("APIFY_API_TOKEN")
 
 class MarketplaceSource:
     def __init__(self, provider):
         self.provider = provider
 
     def listings(self):
-        return [Listing(**item) for item in self.provider()]
+        return [Listing(**item) for item in self.provider() 
