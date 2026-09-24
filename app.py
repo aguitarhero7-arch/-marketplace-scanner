79c8536d23fa217376edb15b0eefd628d4e725d5
aguitarhero7-arch <aguitarhero7@gmail.com>
2026-09-24 05:19:47 +0000

    finish import endpoint

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask,render_template,jsonify
+from flask import Flask,render_template,jsonify,request
+
 from database import connect 
 from sources.manual_import import ManualImportSource
 
@@ -10,5 +11,7 @@ app=Flask(__name__)
 def home(): return render_template('index.html',listings=connect().execute('SELECT * FROM listings ORDER BY first_seen DESC').fetchall())
 @app.get('/api/listings')
 def api(): return jsonify([dict(x) for x in connect().execute('SELECT * FROM listings ORDER BY first_seen DESC').fetchall()])
+@app.post('/api/import')
+def import_listings(): return jsonify({'added':scan(ManualImportSource(request.get_json()))})
 if __name__=='__main__': app.run(host='0.0.0.0',port=5000)
 
